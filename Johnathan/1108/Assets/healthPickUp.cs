3f49187ce8be21700211090de6338a2d93edfe13
jtran93 <johnathan_tran93@yahoo.com>
2016-11-08 02:32:54 -0600

    Added David
    Shooting animation and whatnot

---
+++ Johnathan/1108/Assets/healthPickUp.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class healthPickUp : MonoBehaviour {
+
+    public int healthGained;
+    Player player;
+
+	// Use this for initialization
+	void Start () {
+        player = GetComponent<Player>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+            player.healPlayer(healthGained);
+
+    }
+}
+
+
