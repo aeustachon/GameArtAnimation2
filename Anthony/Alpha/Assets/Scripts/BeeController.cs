a5ec8f81a13c9fd520573a999666bb066e865450
Anthony Eustachon <bamtonio@gmail.com>
2016-11-15 05:26:53 -0600

    Fixed woods Level

---
+++ Anthony/Alpha/Assets/Scripts/BeeController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeeController : MonoBehaviour {
+
+	public int health = 1;
+	public int speed = 10;
+
+	private Transform target;
+
+	void Start () {
+		target = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+
+	void Update () {
+		goToTarget ();
+	}
+
+	void OnTriggerEnter2D(Collider2D otherObject)
+	{
+		if (otherObject.gameObject.tag == "Player" || otherObject.gameObject.tag == "Bullet") {
+			Destroy (gameObject);
+		}
+	}
+
+	void goToTarget()
+	{
+		if (target.transform.position.x > transform.position.x)
+		{
+			if (speed < 0)
+			{
+				speed = -speed;
+			}
+		}
+		else if (target.transform.position.x < transform.position.x)
+		{
+			if (speed > 0)
+			{
+				speed = -speed;
+			}
+		}
+
+		transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+	}
+
+	public void Damage(int dmg)
+	{
+		print ("Bee Damaged...");
+		health = health - dmg;
+		if (health <= 0) {
+			Destroy (gameObject);
+		}
+	}
+}
