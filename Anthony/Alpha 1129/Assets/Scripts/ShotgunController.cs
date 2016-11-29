eea99a0bfcc5fdb891df2b6ed2f684d7d6f96c1e
Anthony Eustachon <bamtonio@gmail.com>
2016-11-29 17:58:29 -0600

    Copied Matt's folder to Anthony's

---
+++ Anthony/Alpha 1129/Assets/Scripts/ShotgunController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotgunController : MonoBehaviour {
+
+	public float attackCD = 1.0f;
+	public int damage;
+	public GameObject rightBullet;
+	public GameObject upperRightBullet;
+	public GameObject lowerRightBullet;
+
+	public GameObject leftBullet;
+	public GameObject upperLeftBullet;
+	public GameObject lowerLeftBullet;
+
+	private bool attacking = false;
+	private float attackTimer = 0;
+
+	private Transform firePoint;
+
+    //All audio stuff
+    public AudioClip audioShotgunShoot;
+    private AudioSource audioSourcePlayer;
+
+    void Start()
+	{
+		firePoint = transform.FindChild("firePoint");
+        audioSourcePlayer = GetComponent<AudioSource>();      //Audio Stuff
+    }
+
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.Space) && !attacking) 
+		{
+			attacking = true;
+			attackTimer = attackCD;
+		}
+
+		if (attacking) 
+		{
+			if (attackTimer > 0) 
+			{
+				attackTimer -= Time.deltaTime;
+			}
+			else 
+			{
+				attacking = false;
+			}
+		}
+	}
+
+	public void attack()
+	{
+		if (attacking == false) {
+            audioSourcePlayer.PlayOneShot(audioShotgunShoot, 1F);       //Shoot Sound
+            attacking = true;
+			attackTimer = attackCD;
+			if (firePoint.transform.position.x > gameObject.transform.position.x) {
+				Instantiate (rightBullet, firePoint.position, Quaternion.identity);
+				Instantiate (upperRightBullet, firePoint.position, Quaternion.identity);
+				Instantiate (lowerRightBullet, firePoint.position, Quaternion.identity);
+			}
+			if (firePoint.transform.position.x < gameObject.transform.position.x) {
+				Instantiate (leftBullet, firePoint.position, Quaternion.identity);
+				Instantiate (upperLeftBullet, firePoint.position, Quaternion.identity);
+				Instantiate (lowerLeftBullet, firePoint.position, Quaternion.identity);
+			}
+		}
+	}
+}
