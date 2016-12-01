fe82c9c84656f95c39d861489aaf566a745c3930
jtran93 <johnathan_tran93@yahoo.com>
2016-12-01 01:40:30 -0600

    Final build

---
+++ Johnathan/FinalBuild/Assets/Scripts/BulletController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour
+{
+    public Vector2 speed;
+	public int damagePerBullet;
+    private Rigidbody2D rb;
+    
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = speed;	
+	}
+	
+	void Update ()
+    {
+        rb.velocity = speed;
+	}
+
+    void OnTriggerEnter2D(Collider2D otherObject)
+    {
+		if (otherObject.gameObject.tag == "Enemy")
+        {
+			otherObject.SendMessageUpwards ("Damage", damagePerBullet);
+			Destroy (gameObject);
+        }
+		if (otherObject.gameObject.tag == "GROUND" || otherObject.gameObject.tag == "EnemyAttack")
+		{
+			Destroy (gameObject);
+		}
+    }
+
+}
