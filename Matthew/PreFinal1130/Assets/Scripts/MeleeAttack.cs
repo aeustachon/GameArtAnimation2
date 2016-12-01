73bd38fad94562fde3756e96741027c8a95d3c19
easthavenmj <easthavenmj@sbcglobal.net>
2016-11-30 22:03:46 -0600

    Vampire Boss Sounds

---
+++ Matthew/PreFinal1130/Assets/Scripts/MeleeAttack.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeleeAttack : MonoBehaviour 
+{
+
+	public Collider2D attackTrigger;
+	public float attackCD = 0.3f;
+
+	private Animator animator;
+	private bool attacking = false;
+	private float attackTimer = 0;
+
+	void Start()
+	{
+		animator = gameObject.GetComponent<Animator> ();
+		attackTrigger.enabled = false;
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.Space) && !attacking) 
+		{
+			attacking = true;
+			attackTimer = attackCD;
+			attackTrigger.enabled = true;
+		}
+			
+		if (attacking) 
+		{
+			if (attackTimer > 0) 
+			{
+				attackTimer -= Time.deltaTime;
+			}
+			else 
+			{
+				attacking = false;
+				attackTrigger.enabled = false;
+			}
+		}
+
+		animator.SetBool ("Attacking", attacking);
+	}
+
+	public void attack()
+	{
+		attacking = true;
+		attackTimer = attackCD;
+		attackTrigger.enabled = true;
+	}
+
+}
