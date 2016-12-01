dcd0ede1f8bc37dbf928f29ef8be22041f96eb70
Anthony Eustachon <bamtonio@gmail.com>
2016-11-30 20:17:20 -0600

    moved folder

---
+++ Anthony/PreFinal1130/Assets/Scripts/HealthUI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthUI : MonoBehaviour {
+	
+    public float texWidth;
+    public float texHeight;
+	public Texture tex;
+
+	private GameObject player1;
+
+	void Start () {
+        texWidth = tex.width/2;
+        texHeight = tex.height/2;
+    }
+
+    void OnGUI()
+    {
+		player1 = GameObject.FindGameObjectWithTag ("Player");
+		Player player = player1.GetComponent<Player> ();
+		if (player.playerStats.health > 0) {
+			Rect posRect = new Rect (0, 0, texWidth / 8 * player.playerStats.health, texHeight);
+			Rect texRect = new Rect (0, 0, 1.0f / 8f * player.playerStats.health, 1.0f);
+			GUI.DrawTextureWithTexCoords (posRect, tex, texRect);
+		}
+
+    }
+}
